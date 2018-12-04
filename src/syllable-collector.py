bef41370e41667b50562dab818e3bbde9451c5f1
Yuchen Li <ychnlgy@gmail.com>
2018-12-03 21:49:53 -0500

    Better arrangement

--- src/syllable-collector.py
+++ src/syllable-collector.py
@@ -66,7 +66,7 @@ def to_spectrogram(audio_slices, rate, n=SAMPLES):
     for slc, ends in slcs:
         f, t, spec = scipy.signal.spectrogram(slc, fs=rate, nperseg=NPERSEG, noverlap=NOVERLAP, nfft=NFFT)
         dt = len(t)/len(slc)
-        conv = numpy.array([int(round(e*dt)) for e in ends])
+        conv = numpy.array([min(int(round(e*dt)), len(t)-1) for e in ends])
         marked = numpy.copy(spec)
         marked[:,conv] = numpy.min(marked)
         yield f, t, spec, marked
